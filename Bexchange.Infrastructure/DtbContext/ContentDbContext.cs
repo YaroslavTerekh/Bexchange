1fadc27309e726958095bd3eb33355f43d73b888
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-08-17 14:16:12 +0300

    Added context and repositories

---
+++ Bexchange.Infrastructure/DtbContext/ContentDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Bexchange.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bexchange.Infrastructure.DtbContext
+{
+    public class ContentDbContext : DbContext
+    {
+        public ContentDbContext(DbContextOptions<ContentDbContext> opts) : base(opts) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<ExchangeOrder> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExchangeOrder>()
+                .HasOne(e => e.FirstBook)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ExchangeOrder>()
+                .HasOne(e => e.SecondBook)
+                .WithMany()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
