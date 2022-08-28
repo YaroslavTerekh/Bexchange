81e92deb2171e2996b9618821bb3949f363f9e11
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-08-28 20:39:20 +0300

    Added custom validation attribute

---
+++ Bexchange.Domain/Annotations/ValidBookAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bexchange.Domain.Annotations
+{
+    public class ValidBookAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value == null || Char.IsUpper(value.ToString()[0]))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- Bexchange.Domain/Models/Book.cs
+++ Bexchange.Domain/Models/Book.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Bexchange.Domain.Annotations;
 using Bexchange.Domain.Enum;
 
 namespace Bexchange.Domain.Models
@@ -16,10 +17,12 @@ namespace Bexchange.Domain.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "'Title' field is empty")]
         [MaxLength(100, ErrorMessage = "Too long title")]
+        [ValidBook(ErrorMessage = "Title must start with big letter")]
         public string Title { get; set; }
         [Required(ErrorMessage = "'Description' field is empty")]
         [MaxLength(500, ErrorMessage = "Too long description (500 max)")]
         [MinLength(50, ErrorMessage = "Too short description (50 min)")]
+        [ValidBook(ErrorMessage = "Description must start with big letter")]
         public string Description { get; set; }
         [Required(ErrorMessage = "No photo uploaded")]
         [ForeignKey(nameof(Image))]
